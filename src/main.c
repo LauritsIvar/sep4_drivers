146226c51e4f43158c06d32e631afb296b8dfecc
LauritsIvar <lauritsivar@gmail.com>
2023-08-23 16:02:22 +0200

    a commit from terminal

--- src/main.c
+++ src/main.c
@@ -1,38 +1,25 @@
 #include "led_hal.h"
-#include "uart.h"
+#include "pc_comm.h"
 #include <util/delay.h>
 #include <stdio.h>
 #include "wifi.h"
 
 
-char received_buffer[128];
-void receive(){
-    uart_send_string_blocking(USART_0, received_buffer);
+void receive(char byte){
+
+  pc_comm_send_string_blocking("0");
+  pc_comm_send_array_blocking((uint8_t*)&byte, 1);
 }
 
+
 int main(){
-
-
-  led_hal_init();
-  uart_init(USART_0, 115200, NULL);
-  wifi_init();
-  wifi_command_disable_echo();
-  wifi_command_set_mode_to_1();
-  wifi_command_set_to_single_Connection();
-  wifi_command_join_AP("Den gamle daarlige router","vildfred");
-  wifi_command_create_TCP_connection("10.0.0.42", 23, receive, received_buffer);
-  
-uint16_t i;
+  pc_comm_init(9600, receive);
   while (1)
   {
-    
-i++;
-wifi_command_TCP_transmit(&i,2);
-_delay_ms(1000);
-
-    
+    _delay_ms(1000);
+    pc_comm_send_string_blocking("Lets Test This!\n");
+    pc_comm_send_array_nonBlocking((uint8_t*)"123456789", 7);
   }
   
-
 
 }
