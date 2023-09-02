8eab98165e8c64583d5d68f9f5343384a59d4dd2
LauritsIvar <lauritsivar@gmail.com>
2023-09-02 20:55:05 +0200

    implemented the lightsensor

--- src/main.c
+++ src/main.c
@@ -2,6 +2,9 @@
 #include "pc_comm.h"
 #include "dht11.h"
 #include "display.h"
+#include "leds.h"
+#include "light.h"
+#include "buzzer.h"
 
 
 int main(void)
@@ -9,26 +12,22 @@ int main(void)
   pc_comm_init(115200, NULL);
     uint8_t h1, h2, t1, t2;
     char str[128];
-    int16_t i=-999;
+    int16_t light;
     display_init();
+    leds_init();
+    light_init();
+    
 
     while (1)
     {
-      display_int(i);
-     i++;
+     light=light_read();
+      display_int(light);
+      _delay_ms(200);
+      sprintf(str,"Light = %d \n", light);
+      pc_comm_send_string_blocking(str);
 
 
-
-      h1 =h2 =t1= t2=0;
-
-        //dht11_get(&h1, &h2, &t1, &t2);
-        _delay_ms(3);
-        /*
-        uint8_t humidity_integer, humidity_decimal, temperature_integer, temperature_decimal;
-        
-        dht11_get(&humidity_integer, &humidity_decimal, &temperature_integer, &temperature_decimal);
-        sprintf(str, "Humidity = %d.%d%% and the temperature = %d.%d C\n\n", humidity_integer, humidity_decimal, temperature_integer, temperature_decimal);
-        pc_comm_send_string_blocking(str);*/
-    }
-    return 0;
-}
+      
+   
+} 
+return 0;}
