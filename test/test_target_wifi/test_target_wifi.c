4bc332e4187cec8fad2e925bfc41845ad785b6e3
LauritsIvar <lauritsivar@gmail.com>
2023-09-30 08:26:22 +0200

    didnt do much

--- test/test_target_wifi/test_target_wifi.c
+++ test/test_target_wifi/test_target_wifi.c
@@ -1,7 +1,7 @@
 // Tests the Wifi module on target
 #define WIFI_SSID "Den gamle daarlige router"
 #define WIFI_PASSWORD "vildfred"
-#define TCP_SERVER "10.0.0.34" // should be running and in echo mode
+#define TCP_SERVER "10.0.0.34" // should be running and in echo mode for the test to pass. 
 
 #include "unity.h"
 #include "wifi.h"
